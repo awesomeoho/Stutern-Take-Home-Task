fb60c3ed95b2896aa9aa244f5f2cf0aa95adf2e3
awesomeoho <olakunleoho@gmail.com>
2021-06-26 00:18:11 +0100

    Create task.js

---
+++ task.js
@@ -0,0 +1,54 @@
+
+
+// 1. Legal Drinking Age in Nigeria
+var output = isOldEnoughToDrink(18);
+    if (output>=18) {alert("True");}
+    else (output<18) {alert("false");}
+    
+console.log(output);
+
+// 2. Old enough to drink and drive in the US
+
+var output = isOldEnoughToDrinkAndDrive();
+    let (output=null)
+    if (output=Number) {alert(False)}
+
+    console.log(output);
+
+// 3. 
+var obj = {
+    name: "Hammed"
+};
+var output = getProperty(obj, "Hammed")
+
+console.log(output);
+
+// 4.
+var sex = {};
+
+addProperty(sex, 'male');
+
+console.log(sex.male);
+
+// 5.
+
+var details = {
+    age: 28
+};
+var output = isPersonOldEnoughToDrinkAndDrive(details);
+    let (output=null)
+    if (output=Number) {alert(False)}
+console.log(output);
+
+// 6.
+var count = computeAverageLengthOfWords('code','programs');
+var n = str.length;
+console.log(count);
+
+// 7.
+let data = ["Pharmacist", "Hammed", "Olakunle", "Ogunlowo"];
+function transformFirstAndLast() {
+    let f=s[0];
+    let l=s[s.length-1]
+}
+console.log(data);
